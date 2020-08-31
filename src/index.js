04f2abb229f8918bfbf128f99f8a9d2cd5c4cbb3
Franzi-4 <67898573+Franzi-4@users.noreply.github.com>
2020-08-31 09:30:10 +0200

    Changed Index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ function displayWeatherCondition(response) {
     response.data.main.temp
   );
 
-
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -50,37 +49,10 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
- {  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-
-{function searchCity(city) 
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
->>>>>>> GitHub
 
-<<<<<<< Codesandbox
-function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-=======
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
->>>>>>> GitHub
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-<<<<<<< Codesandbox
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
@@ -88,38 +60,19 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 66;
-=======
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
->>>>>>> GitHub
-}
-
-<<<<<<< Codesandbox
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 19;
 }
-=======
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Berlin");
-
->>>>>>> GitHub
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
